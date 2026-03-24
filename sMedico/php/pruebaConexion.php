3e9ca0bbe5396fe300308d035da6818b3093da16
LuXyTrsh <163183523+LuXyTrsh@users.noreply.github.com>
2026-03-24 13:18:47 -0600

    Actualizacion php reparada

--- sMedico/php/pruebaConexion.php
+++ sMedico/php/pruebaConexion.php
@@ -2,8 +2,9 @@
 $host = "production.ccjgeakiwlqp.us-east-1.rds.amazonaws.com";
 $usuario = "ian";
 $password = "396925";
-$bd = "ServicioMedicoITLA";
-$conn = mysqli_connect($host, $usuario, $password, $bd);
+$base_datos = "ServicioMedicoITLA";
+include 'var/www/proyects/api/dbconection.php';
+$conn = mysqli_connect($host, $usuario, $password, $base_datos);
 if (!$conn) {
     die("Error de conexión: " . mysqli_connect_error());
 }
