1dccc05fc2b7aafe79995452cac52315ccec81c8
LuXyTrsh <163183523+LuXyTrsh@users.noreply.github.com>
2026-03-24 14:21:48 -0600

    reparacion 8

--- sMedico/php/pruebaConexion.php
+++ sMedico/php/pruebaConexion.php
@@ -4,6 +4,5 @@ $usuario = "ian";
 $password = "396925";
 $base_datos = "ServicioMedicoITLA";
 include ("/var/www/proyects/api/dbconection.php");
-$conn = mysqli_connect($host, $usuario, $password, $base_datos);
 echo "Conexión exitosa a la base de datos.";
 ?>
