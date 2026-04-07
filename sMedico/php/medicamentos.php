659c3ca4d01d33b07aa4a2143a404490eddec807
LuXyTrsh <163183523+LuXyTrsh@users.noreply.github.com>
2026-04-07 14:03:52 -0600

    actualizacion

---
+++ sMedico/php/medicamentos.php
@@ -0,0 +1,16 @@
+<?php
+include("pruebaConexion.php");
+if(isset($_POST['enviar2'])) {
+    $nombre = $_POST['name'];
+    $nombre_generico = $_POST['generic-name'];
+    $tipo_dosis = $_POST['dose'];
+    $cantiddad = $_POST['quantity'];
+    $presenyacion = $_POST['presentation'];
+    $sql1 = "INSERT INTO Medicamentos (medicamento, nombre_generico, dosis, cantidad, presentacion) 
+            VALUES ('$nombre', '$nombre_generico', '$tipo_dosis', '$cantiddad', '$presenyacion')";
+            if (mysqli_query($conn, $sql1)) {
+        echo "Registro exitoso.";
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
+ }
