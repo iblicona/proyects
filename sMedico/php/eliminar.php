ac1d8226c7f5ce7c58c8154c0349aa8fc5222270
iblicona <iblicona@hotmail.com>
2026-04-09 16:14:33 -0600

    Validar ID
    Checar que ID llega

--- sMedico/php/eliminar.php
+++ sMedico/php/eliminar.php
@@ -5,7 +5,8 @@ if(isset($_GET['id_medicamento'])) {
     $id = $_GET['id_medicamento'];
     $eliminar = "DELETE FROM Medicamentos WHERE id_medicamento = $id";
     $resultado = mysqli_query($conn, $eliminar);
-    echo "Registro eliminado correctamente";
+    echo "El id del elemento";
+    echo "Registro eliminado correctamente", $id;
     header("Location: ../medicamentos_inicio.php");
 }
 ?>
