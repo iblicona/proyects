3cdd1681f5136f968207493b5be82122a57d45dc
LuXyTrsh <163183523+LuXyTrsh@users.noreply.github.com>
2026-04-14 13:59:04 -0600

    reparacion 3

---
+++ sMedico/php/utensilios.php
@@ -0,0 +1,15 @@
+<?php
+include("pruebaConexion.php");
+if(isset($_POST['enviarconsulta'])) {
+    $matricula = $_POST['matricula'];
+    $tipo = $_POST['disposable'];
+    $cantidad = $_POST['quantity'];
+    $sql1 = "INSERT INTO Utensilios (matricula, tipo, cantidad) 
+            VALUES ('$matricula', '$tipo', '$cantidad')";
+            if (mysqli_query($conn, $sql1)) {
+        echo "Registro exitoso.";
+        header("Location: ../consulta.html");
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
+ }
